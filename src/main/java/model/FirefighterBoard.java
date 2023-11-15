63d1f421788f1dd6c1c99cc837832754469acbd7
a21230760 <youssef.azizi@etu.univ-amu.fr>
2023-11-15 21:36:34 +0100

    j'ai mise à jour le code

--- src/main/java/model/FirefighterBoard.java
+++ src/main/java/model/FirefighterBoard.java
@@ -7,14 +7,12 @@ import java.util.*;
 
 public class FirefighterBoard implements Board<List<ModelElement>> {
 
-  private Elements<Set<Position>> fire ;
-  private Elements<List<Position>> firefighter;
+  private final Elements<Set<Position>> fire ;
+  private final Elements<List<Position>> firefighter;
   private final int columnCount;
   private final int rowCount;
   private int step = 0;
-  private RandomPositionsGenerator randomPositionsGenerator;
-
-  private Updater updater;
+  private final RandomPositionsGenerator randomPositionsGenerator;
 
 
   public FirefighterBoard(int columnCount, int rowCount, int initialFireCount, int initialFirefighterCount) {
@@ -55,51 +53,20 @@ public class FirefighterBoard implements Board<List<ModelElement>> {
   }
 
   public List<Position> updateToNextGeneration() {
-    this.updater = new FirefighterUpdater(firefighter,fire,columnCount,rowCount);
-    List<Position> modifiedPositions = updater.update();
-    this.updater = new FireUpdater(step,columnCount,rowCount,fire.getPositions());
-    modifiedPositions.addAll(updater.update());
+    Updater updater = new FirefighterUpdater(firefighter, fire, columnCount, rowCount);
+        List<Position> modifiedPositions = updater.update();
+    updater = new FireUpdater(step,columnCount,rowCount,fire.getPositions());
+        modifiedPositions.addAll(updater.update());
     step++;
     return modifiedPositions;
   }
-
-//  private List<Position> updateFires() {
-//    List<Position> modifiedPositions = new ArrayList<>();
-//    if (step % 2 == 0) {
-//      List<Position> newFirePositions = new ArrayList<>();
-//      for (Position fire : fire.getPositions()) {
-//        newFirePositions.addAll(neighbors(fire));
-//      }
-//      fire.getPositions().addAll(newFirePositions);
-//      modifiedPositions.addAll(newFirePositions);
-//    }
-//    return modifiedPositions;
-//
-//  }
 
   @Override
   public int stepNumber() {
     return step;
   }
 
-//  private List<Position> updateFirefighters() {
-//    List<Position> modifiedPosition = new ArrayList<>();
-//    List<Position> firefighterNewPositions = new ArrayList<>();
-//    for (Position firefighterPosition : firefighter.getPositions()) {
-//      Position newFirefighterPosition = neighborClosestToFire(firefighterPosition);
-//      firefighterNewPositions.add(newFirefighterPosition);
-//      extinguish(newFirefighterPosition);
-//      modifiedPosition.add(firefighterPosition);
-//      modifiedPosition.add(newFirefighterPosition);
-//      List<Position> neighborFirePositions = neighbors(newFirefighterPosition).stream()
-//              .filter(fire.getPositions()::contains).toList();
-//      for(Position firePosition : neighborFirePositions)
-//        extinguish(firePosition);
-//      modifiedPosition.addAll(neighborFirePositions);
-//    }
-//    firefighter.setPositions(firefighterNewPositions);
-//    return modifiedPosition;
-//  }
+
 
   @Override
   public void reset() {
@@ -107,34 +74,6 @@ public class FirefighterBoard implements Board<List<ModelElement>> {
     initializeElements();
   }
 
-//  private void extinguish(Position position) {
-//    fire.getPositions().remove(position);
-//  }
-
-/*  private List<Position> neighbors(Position position) {
-    Neighbors neighborsOfPosition = new Neighbors(columnCount,rowCount);
-    return neighborsOfPosition.neighbors(position);
-  }*/
-
- /* private Position neighborClosestToFire(Position position) {
-    Set<Position> seen = new HashSet<>();
-    HashMap<Position, Position> firstMove = new HashMap<>();
-    Queue<Position> toVisit = new LinkedList<>(neighbors(position));
-    for (Position initialMove : toVisit)
-      firstMove.put(initialMove, initialMove);
-    while (!toVisit.isEmpty()) {
-      Position current = toVisit.poll();
-      if (fire.getPositions().contains(current))
-        return firstMove.get(current);
-      for (Position adjacent : neighbors(current)) {
-        if (seen.contains(adjacent)) continue;
-        toVisit.add(adjacent);
-        seen.add(adjacent);
-        firstMove.put(adjacent, firstMove.get(current));
-      }
-    }
-    return position;
-  }*/
 
   @Override
   public void setState(List<ModelElement> state, Position position) {
