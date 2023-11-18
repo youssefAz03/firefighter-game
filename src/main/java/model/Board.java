8cb2989738a02c4387e29ae3efa6d0a7c6c94954
a21230760 <youssef.azizi@etu.univ-amu.fr>
2023-11-18 10:01:49 +0100

    j'ai courrigé une erreur  j'avais dans la method initializeElements() dans la class FirefighterBoard.java

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -11,55 +11,55 @@ import java.util.List;
  */
 public interface Board<S> {
 
-  /**
-   * Get the state of the board at a specific position.
-   *
-   * @param position The position on the board for which to retrieve the state.
-   * @return The state at the specified position.
-   */
-  S getState(Position position);
+    /**
+     * Get the state of the board at a specific position.
+     *
+     * @param position The position on the board for which to retrieve the state.
+     * @return The state at the specified position.
+     */
+    S getState(Position position);
 
-  /**
-   * Set the state of a specific position on the board to the specified state.
-   *
-   * @param state The state to set for the given position.
-   * @param position The position on the board for which to set the state.
-   */
-  void setState(S state, Position position);
+    /**
+     * Set the state of a specific position on the board to the specified state.
+     *
+     * @param state    The state to set for the given position.
+     * @param position The position on the board for which to set the state.
+     */
+    void setState(S state, Position position);
 
-  /**
-   * Get the number of rows in the board.
-   *
-   * @return The number of rows in the board.
-   */
-  int rowCount();
+    /**
+     * Get the number of rows in the board.
+     *
+     * @return The number of rows in the board.
+     */
+    int rowCount();
 
-  /**
-   * Get the number of columns in the board.
-   *
-   * @return The number of columns in the board.
-   */
-  int columnCount();
+    /**
+     * Get the number of columns in the board.
+     *
+     * @return The number of columns in the board.
+     */
+    int columnCount();
 
-  /**
-   * Update the board to its next generation or state. This method may modify the
-   * internal state of the board and return a list of positions that have changed
-   * during the update.
-   *
-   * @return A list of positions that have changed during the update.
-   */
-  List<Position> updateToNextGeneration();
+    /**
+     * Update the board to its next generation or state. This method may modify the
+     * internal state of the board and return a list of positions that have changed
+     * during the update.
+     *
+     * @return A list of positions that have changed during the update.
+     */
+    List<Position> updateToNextGeneration();
 
-  /**
-   * Reset the board to its initial state.
-   */
-  void reset();
+    /**
+     * Reset the board to its initial state.
+     */
+    void reset();
 
-  /**
-   * Get the current step number or generation of the board.
-   *
-   * @return The current step number or generation.
-   */
-  int stepNumber();
+    /**
+     * Get the current step number or generation of the board.
+     *
+     * @return The current step number or generation.
+     */
+    int stepNumber();
 }
 
--- src/main/java/model/FirefighterBoard.java
+++ src/main/java/model/FirefighterBoard.java
@@ -7,85 +7,84 @@ import java.util.*;
 
 public class FirefighterBoard implements Board<List<ModelElement>> {
 
-  private final Elements<Set<Position>> fire ;
-  private final Elements<List<Position>> firefighter;
-  private final int columnCount;
-  private final int rowCount;
-  private int step = 0;
-  private final RandomPositionsGenerator randomPositionsGenerator;
+    private final Elements<Set<Position>> fire;
+    private final Elements<List<Position>> firefighter;
+    private final int columnCount;
+    private final int rowCount;
+    private int step = 0;
+    private final RandomPositionsGenerator randomPositionsGenerator;
 
 
-  public FirefighterBoard(int columnCount, int rowCount, int initialFireCount, int initialFirefighterCount) {
-    this.columnCount = columnCount;
-    this.rowCount = rowCount;
-    this.fire = new Fire(initialFireCount);
-    this.firefighter = new Firefighter(initialFirefighterCount);
-    this.randomPositionsGenerator = new RandomPositionsGenerator(columnCount,rowCount);
-    initializeElements();
-  }
+    public FirefighterBoard(int columnCount, int rowCount, int initialFireCount, int initialFirefighterCount) {
+        this.columnCount = columnCount;
+        this.rowCount = rowCount;
+        this.fire = new Fire(initialFireCount);
+        this.firefighter = new Firefighter(initialFirefighterCount);
+        this.randomPositionsGenerator = new RandomPositionsGenerator(columnCount, rowCount);
+        initializeElements();
+    }
 
-  public void initializeElements() {
-    for (int index = 0; index < fire.getInitialCount(); index++)
-        fire.getPositions().add(randomPositionsGenerator.randomPosition());
-    for (int index = 0; index < firefighter.getInitialCount(); index++)
-        firefighter.getPositions().add(randomPositionsGenerator.randomPosition());
-  }
+    public void initializeElements() {
+        fire.setPositions(new HashSet<>());
+        firefighter.setPositions(new ArrayList<>());
+        for (int index = 0; index < fire.getInitialCount(); index++)
+            fire.getPositions().add(randomPositionsGenerator.randomPosition());
+        for (int index = 0; index < firefighter.getInitialCount(); index++)
+            firefighter.getPositions().add(randomPositionsGenerator.randomPosition());
+    }
 
-  @Override
-  public List<ModelElement> getState(Position position) {
-    List<ModelElement> result = new ArrayList<>();
-    for(Position firefighterPosition : firefighter.getPositions())
-      if (firefighterPosition.equals(position))
-        result.add(ModelElement.FIREFIGHTER);
-    if(fire.getPositions().contains(position))
-      result.add(ModelElement.FIRE);
-    return result;
-  }
+    @Override
+    public List<ModelElement> getState(Position position) {
+        List<ModelElement> result = new ArrayList<>();
+        for (Position firefighterPosition : firefighter.getPositions())
+            if (firefighterPosition.equals(position))
+                result.add(ModelElement.FIREFIGHTER);
+        if (fire.getPositions().contains(position))
+            result.add(ModelElement.FIRE);
+        return result;
+    }
 
-  @Override
-  public int rowCount() {
-    return rowCount;
-  }
+    @Override
+    public int rowCount() {
+        return rowCount;
+    }
 
-  @Override
-  public int columnCount() {
-    return columnCount;
-  }
+    @Override
+    public int columnCount() {
+        return columnCount;
+    }
 
-  public List<Position> updateToNextGeneration() {
-    Updater updater = new FirefighterUpdater(firefighter, fire, columnCount, rowCount);
+    public List<Position> updateToNextGeneration() {
+        Updater updater = new FirefighterUpdater(firefighter, fire, columnCount, rowCount);
         List<Position> modifiedPositions = updater.update();
-    updater = new FireUpdater(step,columnCount,rowCount,fire.getPositions());
+        updater = new FireUpdater(step, columnCount, rowCount, fire.getPositions());
         modifiedPositions.addAll(updater.update());
-    step++;
-    return modifiedPositions;
-  }
-
-  @Override
-  public int stepNumber() {
-    return step;
-  }
-
-
-
-  @Override
-  public void reset() {
-    step = 0;
-    initializeElements();
-  }
-
-
-  @Override
-  public void setState(List<ModelElement> state, Position position) {
-    fire.getPositions().remove(position);
-    for (;;) {
-      if (!firefighter.getPositions().remove(position)) break;
+        step++;
+        return modifiedPositions;
     }
-    for(ModelElement element : state){
-      switch (element){
-        case FIRE -> fire.getPositions().add(position);
-        case FIREFIGHTER -> firefighter.getPositions().add(position);
-      }
+
+    @Override
+    public int stepNumber() {
+        return step;
     }
-  }
+
+    @Override
+    public void reset() {
+        step = 0;
+        initializeElements();
+    }
+
+    @Override
+    public void setState(List<ModelElement> state, Position position) {
+        fire.getPositions().remove(position);
+        for (; ; ) {
+            if (!firefighter.getPositions().remove(position)) break;
+        }
+        for (ModelElement element : state) {
+            switch (element) {
+                case FIRE -> fire.getPositions().add(position);
+                case FIREFIGHTER -> firefighter.getPositions().add(position);
+            }
+        }
+    }
 }
