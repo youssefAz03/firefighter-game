f8a556381786dbae37accbe9626dfcde36d3edf8
belkhalifa <mohamed-amine.bel-khalifa@etu.univ-amu.fr>
2023-11-23 15:03:22 +0100

    j'ai modifier les attributs de cette class pour qu'il prend une board

--- src/main/java/model/AbstractFirefighterUpdater.java
+++ src/main/java/model/AbstractFirefighterUpdater.java
@@ -5,48 +5,42 @@ import util.Position;
 import java.util.*;
 
 public abstract class AbstractFirefighterUpdater implements Updater{
-    private AbstractFirefighter firefighter;
-    private Elements<Set<Position>> fire;
-    private final int columnCount;
-    private final int rowCount;
+    private final Board board;
 
-    public AbstractFirefighterUpdater(AbstractFirefighter firefighter, Elements<Set<Position>> fire, int columnCount, int rowCount) {
-        this.firefighter = firefighter;
-        this.fire = fire;
-        this.columnCount = columnCount;
-        this.rowCount = rowCount;
+    public AbstractFirefighterUpdater(Board board) {
+        this.board = board;
     }
     @Override
     public List<Position> update() {
-        Neighbors neighbors = new Neighbors(columnCount,rowCount);
+        Neighbors neighbors = new Neighbors(board.columnCount(), board.rowCount());
         List<Position> modifiedPosition = new ArrayList<>();
         List<Position> firefighterNewPositions = new ArrayList<>();
 
-        for (Position firefighterPosition : firefighter.getPositions()) {
+        for (Position firefighterPosition : board.getFirefighter().getPositions()) {
             Position newFirefighterPosition = neighborClosestToFire(firefighterPosition);
             firefighterNewPositions.add(newFirefighterPosition);
             extinguish(newFirefighterPosition);
             modifiedPosition.add(firefighterPosition);
             modifiedPosition.add(newFirefighterPosition);
             List<Position> neighborFirePositions = neighbors.neighbors(newFirefighterPosition).stream()
-                    .filter(fire.getPositions()::contains).toList();
+                    .filter(board.getFire().getPositions()::contains).toList();
             for(Position firePosition : neighborFirePositions)
                 extinguish(firePosition);
             modifiedPosition.addAll(neighborFirePositions);
         }
-        firefighter.setPositions(firefighterNewPositions);
+        board.getFirefighter().setPositions(firefighterNewPositions);
         return modifiedPosition;
     }
     public Position neighborClosestToFire(Position position) {
         Set<Position> seen = new HashSet<>();
-        Neighbors neighborsOfPosition = new Neighbors(columnCount,rowCount);
+        Neighbors neighborsOfPosition = new Neighbors(board.columnCount(), board.rowCount());
         HashMap<Position, Position> firstMove = new HashMap<>();
         Queue<Position> toVisit = new LinkedList<>(neighborsOfPosition.neighbors(position));
         for (Position initialMove : toVisit)
             firstMove.put(initialMove, initialMove);
         while (!toVisit.isEmpty()) {
             Position current = toVisit.poll();
-            if (fire.getPositions().contains(current))
+            if (board.getFire().getPositions().contains(current))
                 return firstMove.get(current);
             for (Position adjacent : neighborsOfPosition.neighbors(current)) {
                 if (seen.contains(adjacent)) continue;
@@ -58,6 +52,6 @@ public abstract class AbstractFirefighterUpdater implements Updater{
         return position;
     }
     public void extinguish(Position position) {
-        fire.getPositions().remove(position);
+        board.getFire().getPositions().remove(position);
     }
 }
